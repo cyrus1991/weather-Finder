2b727f75db53729f579d066ede7aad11fec88e14
CYRUS <Cyrus.mohammadi@lokalleads.de>
2018-07-11 13:34:25 +0200

    titles are ready

---
+++ src/components/Titles.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+export default class  Titles extends  React.Component {
+
+render ()  {
+
+    return(
+
+            <div>
+               <h1>Weather Finder</h1>  
+               <p>Find out temporature, Condition and more...</p> 
+
+            </div>
+
+
+
+    )
+
+}
+
+
+
+}
